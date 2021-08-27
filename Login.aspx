5d465cf8655ed1bbc471431e6c3dd4a2e2d42731
Moni <malolepszamonika@gmail.com>
2021-08-27 12:24:09 +0200

    save 27/08

---
+++ Login.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Title="Login" MasterPageFile="~/Base.Master" CodeBehind="Login.aspx.cs" Inherits="Tournament_Management.Home" %>
+
+<%--<asp:Content ID="Content1" ContentPlaceHolderID="Home" runat="server">
+    <div class="jumbotron">
+        <span>
+            <a runat="server" href="~/Home">
+                <img class="img-responsive" src="Static/Images/Board-Background.jpg" />
+            </a>
+        </span>
+        <h1 class="display-4">Hello,</h1>
+        <p class="lead">Please provide your username and password.</p>
+        <p>Not a user yet? Register below:</p>
+                    <a type="button" class="btn btn-info btn-lg" runat="server" href="~/View/Register">I'm new here</a>
+        <hr class="my-4">
+        <p></p>
+        <br />
+        <div> 
+
+            <a type="button" class="btn btn-info btn-lg" runat="server" href="~/View/TournamentManagement">go to tournaments</a>
+        </div>
+    </div>
+
+</asp:Content>--%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<script runat="server">
+bool IsValidEmail(string strIn)
+{
+    // Return true if strIn is in valid email format.
+    return Regex.IsMatch(strIn, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"); 
+}
+
+void OnLoggingIn(object sender, System.Web.UI.WebControls.LoginCancelEventArgs e)
+{
+    if (!IsValidEmail(Login1.UserName))
+    {
+        Login1.InstructionText = "Enter a valid email address.";
+        Login1.InstructionTextStyle.ForeColor = System.Drawing.Color.RosyBrown;
+        e.Cancel = true;
+    }
+    else
+    {
+        Login1.InstructionText = String.Empty;
+    }
+}
+
+void OnLoginError(object sender, EventArgs e)
+{
+    Login1.HelpPageText = "Help with logging in...";
+    Login1.PasswordRecoveryText = "Forgot your password?";
+}
+</script>
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+    <head runat="server">
+    <title>ASP.NET Example</title>
+</head>
+<body>
+        <form id="loginForm" runat="server">
+            <asp:Login id="Login1" runat="server" 
+                BorderStyle="Solid" 
+                BackColor="#F7F7DE" 
+                BorderWidth="1px"
+                BorderColor="#CCCC99" 
+                Font-Size="10pt" 
+                Font-Names="Verdana" 
+                CreateUserText="Create a new user..."
+                CreateUserUrl="newUser.aspx" 
+                HelpPageUrl="help.aspx"
+                PasswordRecoveryUrl="getPass.aspx" 
+                UserNameLabelText="Email address:" 
+                OnLoggingIn="OnLoggingIn"
+                OnLoginError="OnLoginError" >
+                <TitleTextStyle Font-Bold="True" 
+                    ForeColor="#FFFFFF" 
+                    BackColor="#6B696B">
+                </TitleTextStyle>
+            </asp:Login>
+
+        </form>
+    </body>
+</html>
---
+++ Model/User.cs
@@ -0,0 +1,198 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tournament_Management.Model
+{
+    public class User : Person
+    {
+        
+        #region Attributes
+
+        private string _username;
+        private string _passwort;
+        private int _role;
+
+        #endregion
+
+
+        #region Properties
+        public string Passwort { get => _passwort; set => _passwort = value; }
+        public int Role { get => _role; set => _role = value; }
+        public string Username { get => _username; set => _username = value; }
+
+        #endregion
+
+        #region Constructors
+
+        public User()
+        {
+            this.Username = "";
+            this.Passwort = "";
+            this.Role = 1;
+        }
+
+        public User(string name, bool active, string surname, string un, string pass, int r) : base(name, surname, active)
+        {
+            this.Passwort = pass;
+            this.Role = r;
+            this.Username = un;
+
+        }
+
+        public User(string username, string passwort)
+        {
+            this.Passwort = passwort;
+            this.Role = 1;
+            this.Username = username;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void Update()
+        {
+            MySqlConnection con = new MySqlConnection("Server=127.0.0.1;Database=tournament;Uid=user;Pwd=user;");
+
+            con.Open();
+            MySqlTransaction transaction = con.BeginTransaction();
+
+            try
+            {
+
+            /*    string updatePerson = $"UPDATE PERSON SET name='{Name}', age='{Age}', surname='{Surname}', active='{Active}' WHERE ID='{Id}'";
+                string updateTrainer = $"UPDATE TRAINER SET type_id='{Type}' WHERE PERSON_ID= '{Id}'";
+
+                MySqlCommand cmd = new MySqlCommand()
+                {
+                    Connection = con,
+                    Transaction = transaction
+                };
+
+                cmd.CommandText = updatePerson;
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = updateTrainer;
+                cmd.ExecuteNonQuery();
+
+                transaction.Commit();
+                */
+
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public override void Put()
+        {
+            MySqlConnection con = new MySqlConnection("Server=127.0.0.1;Database=tournament;Uid=user;Pwd=user;");
+
+            con.Open();
+            MySqlTransaction transaction = con.BeginTransaction();
+
+            try
+            {
+
+                string insertParticipant = $"INSERT INTO PERSON (name, surname, age, active) VALUES ('{Name}', '{Surname}', '{Age}', '{Active}')";
+
+
+                MySqlCommand cmd = new MySqlCommand()
+                {
+                    Connection = con,
+                    Transaction = transaction
+                };
+                
+                /*
+                cmd.CommandText = insertParticipant;
+                cmd.ExecuteNonQuery();
+                int person_id = (int)cmd.LastInsertedId;
+                string insertTrainer = $"INSERT INTO TRAINER (team_id, person_id, type_id) VALUES('1', '{person_id}', '{Type}')";
+                cmd.CommandText = insertTrainer;
+                cmd.ExecuteNonQuery();
+
+
+                transaction.Commit();
+                */
+
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+            }
+            finally
+            {
+                con.Close();
+
+            }
+        }
+
+        public override void Delete()
+        {
+            MySqlConnection con = new MySqlConnection("Server=127.0.0.1;Database=tournament;Uid=user;Pwd=user;");
+
+            try
+            {
+                con.Open();
+
+                string query = $"DELETE FROM PERSON WHERE ID = '{Id}'";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+
+            }
+        }
+
+        public override void Get(int id)
+        {
+            MySqlConnection con = new MySqlConnection("Server=127.0.0.1;Database=tournament;Uid=user;Pwd=user;");
+
+            try
+            {
+                con.Open();
+                string query = $"SELECT * FROM PERSON P JOIN USER U ON P.ID = U.PERSON_ID  WHERE P.ID = '{id}'";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Id = reader.GetInt32("id");
+                    Name = reader.GetString("name");
+                    Username = reader.GetString("username");
+                    Surname = reader.GetString("surname");
+                    Active = reader.GetBoolean("active");
+                    Age = reader.GetInt32("age");
+                    Role = reader.GetInt32("role");
+                }
+
+                reader.Close();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        
+        #endregion
+    }
+}
